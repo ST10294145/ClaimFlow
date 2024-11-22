e56f44285ffc611466b24171b9da5ac0d6c4cc19
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-11-22 18:58:37 +0200

    Added new Migrations These new migrations helped add new coloums to the users table in the database.

--- ClaimFlow/Data/ApplicationDbContext.cs
+++ ClaimFlow/Data/ApplicationDbContext.cs
@@ -11,5 +11,7 @@ namespace ClaimFlow.Data
         }
 
         public DbSet<SalesLeadEntity> SalesLead { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
