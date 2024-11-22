c690b60fa5a1242e0a47bc0ef21025d5c0163007
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-11-22 18:05:35 +0200

    Added AppRoles Controller This controller will help with the roles of the lectuter,HR, and Admin.

--- ClaimFlow/Program.cs
+++ ClaimFlow/Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("FlowDBCnn")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>().AddDefaultTokenProviders()
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
