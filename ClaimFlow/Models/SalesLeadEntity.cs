9cb5dac04fcd6e4ec6bc693eaec89cdb42956484
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-11-22 12:22:30 +0200

    Made model for when user registers an account. This modell will capture their details .

---
+++ ClaimFlow/Models/SalesLeadEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ClaimFlow.Models
+{
+    public class SalesLeadEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+    }
+}
