cca3620b2cc4d69eabdf8d0ee7a0196894c16b39
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-11-22 12:34:16 +0200

    Created DbContext I put this context folder into the new data folder

---
+++ ClaimFlow/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ClaimFlow.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClaimFlow.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SalesLeadEntity> SalesLead { get; set; }
+    }
+}
